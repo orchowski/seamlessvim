714ddb5c9051a0427d4f8be9279b13677d7f457d
Aleksander Orchowski <orchowskialeksander@gmail.com>
2025-08-19 19:42:42 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!bin/bash
+prompt_install() {
+	echo -n "$1 is not installed. Would you like to install it? (y/n) " >&2
+	old_stty_cfg=$(stty -g)
+	stty raw -echo
+	answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+	stty $old_stty_cfg && echo
+	if echo "$answer" | grep -iq "^y" ;then
+		# This could def use community support
+		if [ -x "$(command -v apt-get)" ]; then
+			sudo apt-get install $1 -y
+
+		elif [ -x "$(command -v brew)" ]; then
+			brew install $1
+
+		elif [ -x "$(command -v zypper)" ]; then
+			sudo zypper install $1
+
+		elif [ -x "$(command -v pkg)" ]; then
+			sudo pkg install $1
+
+		elif [ -x "$(command -v pacman)" ]; then
+			sudo pacman -S $1
+
+		else
+			echo "I'm not sure what your package manager is! Please install $1 on your own and run this deploy script again. Tests for package managers are in the deploy script you just ran starting at line 13. Feel free to make a pull request at https://github.com/parth/dotfiles :)" 
+		fi 
+	fi
+}
+
+check_for_software() {
+	echo "Checking to see if $1 is installed"
+	if ! [ -x "$(command -v $1)" ]; then
+		prompt_install $1
+	else
+		echo "$1 is installed."
+	fi
+}
+
+check_for_software nvim
+check_for_software gcc
+check_for_software fzf
+check_for_software luarocks
+
+if [ -d "~/.config/nvim" ]; then
+	tar -czf ~/.config/nvim-$(date +%Y%m%d%H%M%S).tar.gz -C ~/.config/nvim .
+fi
+
+
