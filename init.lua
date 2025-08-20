fdaa056725f2fe243f67faa66268ed54cda0ab39
Aleksander Orchowski <orchowskialeksander@gmail.com>
2025-08-20 08:24:16 +0200

    venv selector disabling, adding line numbers

--- init.lua
+++ init.lua
@@ -99,7 +99,7 @@ vim.g.have_nerd_font = false
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
--- vim.o.number = true
+vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
