669c5624989189295f45c84fb3d1f7eec8ef9474
Aleksander Orchowski <orchowskialeksander@gmail.com>
2025-12-08 23:14:29 +0100

    theme update

--- init.lua
+++ init.lua
@@ -913,6 +913,7 @@ require('lazy').setup({
 
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   { 'shaunsingh/nord.nvim', name = 'nord', priority = 1000 },
+  { 'caglartoklu/borlandp.vim', name = 'borlandp', priority = 1000 },
   {
     'neanias/everforest-nvim',
     version = false,
@@ -944,7 +945,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'everforest'
+      vim.cmd.colorscheme 'borlandp'
     end,
   },
 
@@ -967,7 +968,7 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- require('mini.surround').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
